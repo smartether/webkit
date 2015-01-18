5342889d7a137f44b9548205921585b8ae21135e
Yavor Georgiev <fealebenpae@gmail.com>
2015-01-18 19:57:29 +0200

    Update the preprocessor definitions for WinRT

--- Source/cmake/OptionsWinRT.cmake
+++ Source/cmake/OptionsWinRT.cmake
@@ -3,11 +3,12 @@ include(OptionsWindows)
 add_definitions(/D_WIN32_WINNT=0x0603 /DWINVER=0x0603) # Windows 8.1
 
 if (MSVC_CXX_ARCHITECTURE_ID STREQUAL ARM)
-	add_definitions(/DWTF_CPU_ARM_THUMB2 /DWTF_CPU_ARM_NEON)
+    # We always run in Thumb2, with NEON and VFP, because Microsoft says so:
+    # http://msdn.microsoft.com/en-us/library/dn736986.aspx
+	add_definitions(/DWTF_CPU_ARM_THUMB2 /DWTF_CPU_ARM_NEON /DWTF_CPU_ARM_VFP)
 endif ()
 
 set(ENABLE_LLINT_C_LOOP ON)
 set(ENABLE_JIT OFF)
 set(WTF_USE_ICU_UNICODE OFF)
 set(WTF_USE_NLS_UNICODE ON)
-add_definitions(/DENABLE_ASSEMBLER=0 /DENABLE_YARR_JIT=0)
