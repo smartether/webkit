aee6cbcf819eb052ac96eb7593a4bf8faf1599d8
Yavor Georgiev <fealebenpae@gmail.com>
2015-01-19 14:43:59 +0200

    Only enable YARR JIT if JSC JIT is enabled.

--- Source/WTF/wtf/Platform.h
+++ Source/WTF/wtf/Platform.h
@@ -825,7 +825,7 @@
 #define ENABLE_REGEXP_TRACING 0
 
 /* Yet Another Regex Runtime - turned on by default for JIT enabled ports. */
-#if !defined(ENABLE_YARR_JIT)
+#if !defined(ENABLE_YARR_JIT) && ENABLE(JIT)
 #define ENABLE_YARR_JIT 1
 
 /* Setting this flag compares JIT results with interpreter results. */
